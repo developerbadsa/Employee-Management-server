137eabe5bb9e67e1c653150a9fb57fd8cb429c20
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-11-25 23:14:43 +0600

    jwt token generated in localstorage post done

--- index.js
+++ index.js
@@ -1,40 +1,79 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
-const port = 3000; 
+const port = 3000;
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const {MongoClient, ServerApiVersion} = require('mongodb');
 const uri = process.env.DB_URL;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-
 const client = new MongoClient(uri, {
-serverApi: {
-version: ServerApiVersion.v1,
-strict: true,
-deprecationErrors: true,
-}
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 async function run() {
-try {
+  try {
 
-  app.get('/', (req, res) => {
-        res.send('Hello, World!');
-      });
-
-// Send a ping to confirm a successful connection
-await client.db("admin").command({ ping: 1 });
-console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      const usersDb = client.db("usersDB").collection("usersCollection")
 
 
-} finally {
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+            res.send({ token });
+          })
 
-}
+
+
+      // post requiests
+      app.post('/users', async(req, res)=>{
+            const user = req.body;
+            user.isVerify = false
+            
+            const result = await usersDb.insertOne(user)
+            
+            console.log(user)
+
+            res.send(result)
+
+
+
+      })
+
+
+
+
+    app.get('/', (req, res) => {
+      res.send('Hello, World!');
+    })
+
+
+    await client.db('admin').command({ping: 1});
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+  }
 }
 run().catch(console.dir);
 
 // Start the server
 app.listen(port, () => {
-console.log(`Server is running on${port}`);
+  console.log(`Server is running on${port}`);
 });
